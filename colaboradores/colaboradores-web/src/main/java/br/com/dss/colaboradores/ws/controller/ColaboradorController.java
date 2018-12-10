a99d8a6759c9f7532fc2264f9dabe49896fe3e81
Dione de Souza Silva <dione2508@gmail.com>
2018-12-10 16:45:10 -0200

    Correção do response.

--- colaboradores/colaboradores-web/src/main/java/br/com/dss/colaboradores/ws/controller/ColaboradorController.java
+++ colaboradores/colaboradores-web/src/main/java/br/com/dss/colaboradores/ws/controller/ColaboradorController.java
@@ -2,12 +2,9 @@ package br.com.dss.colaboradores.ws.controller;
 
 
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
